19cec72ef845f426727e2bfe2e64a968de475d8c
JulianaSobaJava <julianasobajava23@gmail.com>
2022-04-27 08:59:12 +0100

    first heroku commit

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: "https://­app-portalapi.herokua­pp.com/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(async (config) => {
