9d01a529d97ba6b21d07cf31153f8230024bf368
Jose Gonçalves <goncalves2541@gmail.com>
2022-04-29 15:33:51 +0100

    solve broken build

--- src/components/mapaView/index.jsx
+++ src/components/mapaView/index.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-} from "https://cdn.esm.sh/react-leaflet";
+import React from 'react';
+// import {
+//   MapContainer,
+//   TileLayer,
+//   Marker,
+//   Popup,
+// } from "react-leaflet";
 
 function MapaView() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <h3>[FUTURE] - Map View Area</h3>
+    // <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    //   <TileLayer
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    //   <Marker position={[51.505, -0.09]}>
+    //     <Popup>
+    //       A pretty CSS3 popup. <br /> Easily customizable.
+    //     </Popup>
+    //   </Marker>
+    // </MapContainer>
   );
 }
 
